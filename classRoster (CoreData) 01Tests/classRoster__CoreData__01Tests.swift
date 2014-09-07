80922e75b499e7395d62f39e8dcd4c7e7b52e555
Jeff Chavez <Maluu@Jeffs-MacBook-Pro.local>
2014-09-06 21:42:52 -0700

    Initial Commit

---
+++ classRoster (CoreData) 01Tests/classRoster__CoreData__01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  classRoster__CoreData__01Tests.swift
+//  classRoster (CoreData) 01Tests
+//
+//  Created by Jeff Chavez on 9/6/14.
+//  Copyright (c) 2014 Jeff Chavez. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class classRoster__CoreData__01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
